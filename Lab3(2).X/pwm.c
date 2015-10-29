6ea9e04f81903ecb1b0b2162446f95fcd2811cb5
Jesse <Jesse@Jesse>
2015-10-29 10:38:52 -0700

    First commit

--- Lab3(2).X/pwm.c
+++ Lab3(2).X/pwm.c
@@ -8,11 +8,15 @@
 #include <xc.h>
 
 
-void initPWM(){
+void initPWM()
+{
     RPD1Rbits.RPD1R = 0b1011; // map OC2 to RD1
+    RPD5Rbits.RPD5R = 0b1100; // map OC1 to RD5
     OC2CON = 0x0000; // Turn off OC1 while doing setup.
     OC2R = 0x0000; // Initialize primary Compare Register
     OC2RS = 0x0000; // Initialize secondary Compare Register
-    OC2CON = 0x0006; // Configure for PWM mode
-    OC2CONSET = 0x8000; // Enable OC1
+    OC2CON = 0x0006;// Configure for PWM mode
+    OC2CONbits.OCTSEL = 1; // Oscillator
+    OC2CONbits.ON = 1; // Enable OC1
 }
+
